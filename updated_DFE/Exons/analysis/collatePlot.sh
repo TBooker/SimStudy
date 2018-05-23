cc7a89fd6a35c15d824916b86d1460a57bae5ced
TBooker <T.R.Booker@sms.ed.ac.uk>
2018-05-23 22:36:08 +0100

    added more csvs

---
+++ updated_DFE/Exons/analysis/collatePlot.sh
@@ -0,0 +1,3 @@
+python ~/project/15.Estimate_Selection_From_Trough/bin/analyse_sfs_from_directory_geneticDistance.py -i ../full_usfs/BGS/sfsGenetic/ -o BGS.full_usfs.Genetic.csv --gz &
+python ~/project/15.Estimate_Selection_From_Trough/bin/analyse_sfs_from_directory_geneticDistance.py -i ../full_usfs/BGS+SWEEP/sfsGenetic/ -o BGS+SWEEP.full_usfs.Genetic.csv  --gz &
+python ~/project/15.Estimate_Selection_From_Trough/bin/analyse_sfs_from_directory_geneticDistance.py -i ../full_usfs/SWEEP/sfsGenetic/ -o SWEEP.full_usfs.Genetic.csv -r 0 --gz
