d67bb8267e57f46ded7268c3743822d702617785
TBooker <T.R.Booker@sms.ed.ac.uk>
2018-06-05 18:48:50 +0100

    added plotting files

---
+++ updated_DFE/CNEs/analysis/collatePlot.sh
@@ -0,0 +1,12 @@
+python ~/project/15.Estimate_Selection_From_Trough/bin/analyse_sfs_from_directory_geneticDistance.py -i ../Ne-anc/BGS/sfsGenetic/ -o BGS.Ne-anc.Genetic.csv --gz --cne &
+
+python ~/project/15.Estimate_Selection_From_Trough/bin/analyse_sfs_from_directory_geneticDistance.py -i ../Ne-anc/BGS+SWEEP/sfsGenetic/ -o BGS+SWEEP.Ne-anc.Genetic.csv  --gz --cne 
+
+python ~/project/15.Estimate_Selection_From_Trough/bin/analyse_sfs_from_directory_geneticDistance.py -i ../Ne-anc/SWEEP/sfsGenetic/ -o SWEEP.Ne-anc.Genetic.csv --gz --cne &
+
+
+python ~/project/15.Estimate_Selection_From_Trough/bin/analyse_sfs_from_directory_geneticDistance.py -i ../full_usfs/BGS/sfsGenetic/ -o BGS.full_usfs.Genetic.csv --gz --cne 
+
+python ~/project/15.Estimate_Selection_From_Trough/bin/analyse_sfs_from_directory_geneticDistance.py -i ../full_usfs/BGS+SWEEP/sfsGenetic/ -o BGS+SWEEP.full_usfs.Genetic.csv  --gz --cne &
+
+python ~/project/15.Estimate_Selection_From_Trough/bin/analyse_sfs_from_directory_geneticDistance.py -i ../full_usfs/SWEEP/sfsGenetic/ -o SWEEP.full_usfs.Genetic.csv --gz --cne
